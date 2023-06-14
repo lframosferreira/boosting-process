4c6e3893624fb2576282ebd1ea3e818dd2189b38
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-06-14 17:34:56 -0300

    added vampire dataset for benchmarking

--- handle_data.sh
+++ handle_data.sh
@@ -4,6 +4,10 @@ if [ ! -d "data/" ]; then
     mkdir data;
 fi
 
-wget -P data/ https://archive.ics.uci.edu/static/public/101/tic+tac+toe+endgame.zip
-unzip data/tic+tac+toe+endgame.zip -d data/
-rm data/tic+tac+toe+endgame.zip
+if [ ! -d "data/tictactoe" ]; then
+    mkdir data/tictactoe;
+fi
+
+wget -P data/tictactoe https://archive.ics.uci.edu/static/public/101/tic+tac+toe+endgame.zip
+unzip data/tictactoe/tic+tac+toe+endgame.zip -d data/tictactoe/
+rm data/tictactoe/tic+tac+toe+endgame.zip
